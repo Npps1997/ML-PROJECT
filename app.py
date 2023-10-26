41b3ece76159d894e89637ec88fa7c03005cd487
Neeraj Kumar Paikra <neeraj.naive.np@gmail.com>
2023-10-26 11:34:20 +0530

    data transformation

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ sys.path.append(r'G:\ML Project\src')
 from mlproject.logger import logging
 from mlproject.exception import CustomException
 from mlproject.component.data_ingestion import DataIngestion
+from mlproject.component.data_transformation import DataTransformation
 
 if __name__ == "__main__":
     logging.info("The execution has started")
@@ -11,7 +12,10 @@ if __name__ == "__main__":
     try:
         # data_ingstion_config = DataIngestionConfig()
         data_ingestion = DataIngestion()
-        data_ingestion.initiate_data_ingestion()
+        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
+
+        data_transformation = DataTransformation()
+        data_transformation.initiate_data_transormation(train_data_path,test_data_path)
 
     except Exception as e:
         logging.info("Custom Exception")
